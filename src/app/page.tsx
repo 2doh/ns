349ffc82dcd321a6330e8b17b8afc1138c006aec
2doh <ldh0973@naver.com>
2025-06-03 15:23:26 +0900

    docs : supabase 추가 및 yup 삭제. zod 추가예정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-"use client";
-
 import AuthForm from "@/components/auth/AuthForm";
 import AuthLink from "./AuthLink";
 import SocialAuth from "./SocialAuth";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { useForm } from "react-hook-form";
 
 export default function Home() {
   const labelArr = [
@@ -14,37 +9,6 @@ export default function Home() {
   ];
   const titleObj = { title: "로그인" };
 
-  const initAuthState = {
-    userMail: "",
-    userPass: "",
-  };
-
-  const AuthSchema = yup.object().shape({
-    userMail: yup
-      .string()
-      .required("이메일을 입력해주세요")
-      .matches(
-        /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
-        "유효한 이메일 형식이 아닙니다.",
-      ),
-    userPass: yup
-      .string()
-      .required("비밀번호를 입력해주세요.")
-      .matches(
-        /^[a-zA-Z0-9]{8,12}$/,
-        "비밀번호는 8~12자 영어와 숫자의 조합으로 입력해 주세요.",
-      ),
-  });
-
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm({
-    defaultValues: initAuthState,
-    resolver: yupResolver(AuthSchema),
-    mode: "onChange",
-  });
   return (
     <>
       <h2
@@ -53,13 +17,7 @@ export default function Home() {
       >
         {titleObj.title}
       </h2>
-      <AuthForm
-        labelArr={labelArr}
-        titleObj={titleObj}
-        register={register}
-        handleSubmit={handleSubmit}
-        errors={errors}
-      />
+      <AuthForm labelArr={labelArr} titleObj={titleObj} />
       <AuthLink />
       <div className="text-center text-sm text-gray-500">
         또는 소셜 계정으로 로그인
--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { FieldErrors, UseFormRegister } from "react-hook-form";
 import FormBtn from "../common/FormBtn";
 
 type LabelItem = {
@@ -7,43 +6,22 @@ type LabelItem = {
   type?: string;
 };
 
-type FormValues = {
-  userMail: string;
-  userPass: string;
-};
-
 type AuthFormProps = {
   labelArr: LabelItem[];
   titleObj: {
     title: string;
   };
-  register: UseFormRegister<FormValues>;
-  handleSubmit: (
-    onValid: (data: FormValues) => void,
-  ) => (e?: React.BaseSyntheticEvent) => void;
-  errors: FieldErrors<FormValues>;
 };
 
-const AuthForm = ({
-  labelArr,
-  titleObj,
-  register,
-  handleSubmit,
-  errors,
-}: AuthFormProps) => {
-  const handleOnSubmit = (data: FormValues) => {
-    console.log(data);
-  };
-
+const AuthForm = ({ labelArr, titleObj }: AuthFormProps) => {
   return (
-    <form className="space-y-4" onSubmit={handleSubmit(handleOnSubmit)}>
+    <form className="space-y-4">
       {labelArr.map((item, index) => (
         <div key={index}>
           <label className="block text-sm mb-1" style={{ color: "#2a1b07" }}>
             {item.label}
           </label>
           <input
-            {...register(item.type === "email" ? "userMail" : "userPass")}
             type={item.type}
             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2"
             style={{
@@ -53,11 +31,7 @@ const AuthForm = ({
           />
         </div>
       ))}
-      <div>
-        {errors?.userMail && (
-          <p className="text-red-500 text-sm">{errors.userMail.message}</p>
-        )}
-      </div>
+
       <FormBtn title={titleObj.title} />
     </form>
   );
